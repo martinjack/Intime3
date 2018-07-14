54748147da061d48d9fa31bb2e32bae15473eb2d
Evgen Kytonin <killfess@gmail.com>
2018-07-14 14:54:44 +0300

    fix get_locality_filter

--- example.php
+++ example.php
@@ -4,6 +4,6 @@ use InTime\InTime3;
 
 include_once 'vendor/autoload.php';
 
-$intime = new InTime3('69502396190001818247');
+$intime = new InTime3('API_KEY');
 
 print_r($intime->get_country_list());
